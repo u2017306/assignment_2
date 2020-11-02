233994f7fd1f95b11de73668c4cbf23fc76c756b
u2017306 <72963854+u2017306@users.noreply.github.com>
2020-11-02 11:21:37 +0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Assignment 2 Completed!
+          11/2/20
         </p>
         <a
           className="App-link"
